9abc692debf4188d473f7ce83dbb46d67e7490e8
KWouterFokkema <k.w.fokkema@utwente.nl>
2022-02-28 15:57:33 +0100

    Permutation approach

--- scheduler.py
+++ scheduler.py
@@ -76,6 +76,8 @@ def solve(instance):
             model.addConstr((order_var == 1) >> (starting_times_vars[(machine, job_1)] + instance.processing_times[(machine, job_1)] <= starting_times_vars[(machine, job_2)]))
             model.addConstr((order_var == 0) >> (starting_times_vars[(machine, job_2)] + instance.processing_times[(machine, job_2)] <= starting_times_vars[(machine, job_1)]))
 
+    # TODO: the order of the first machine is the order of the second machine, and idem for the last two machines.
+
     # The makespan needs to be equal to the finishing time of the last job
     last_machine = instance.machines[-1]
     for job in instance.jobs:
@@ -168,6 +170,107 @@ def solve_ordered(instance):
     return starting_times, False  # The second parameter is False because the solution is heuristic and not necessarily optimal
 
 
+# The following works, but is very slow.
+def solve_permutation(instance):
+    model = gb.Model()
+
+    # Add variables to the model
+    # We make a permutation of the jobs, called virtual_jobs.
+    # On each machine, the first virtual_job is processed before the second, etc.
+
+    job_permutation_vars = {}
+    virtual_processing_time_vars = {}
+    virtual_starting_time_vars = {}
+
+    makespan_var = model.addVar(obj=1)
+
+    for machine in instance.machines:
+        for virtual_job in instance.jobs:
+            for job in instance.jobs:
+                job_permutation_vars[(machine, job, virtual_job)] = model.addVar(vtype=gb.GRB.BINARY)
+
+    for machine in instance.machines:
+        for virtual_job in instance.jobs:
+            virtual_processing_time_vars[(machine, virtual_job)] = model.addVar()
+
+    for machine in instance.machines:
+        for virtual_job in instance.jobs:
+            virtual_starting_time_vars[(machine, virtual_job)] = model.addVar()
+
+    # Starting times in terms of original jobs
+    starting_time_vars = {}
+    for machine in instance.machines:
+        for job in instance.jobs:
+            starting_time_vars[(machine, job)] = model.addVar()
+
+    model.update()
+
+    for machine in instance.machines:
+        for job in instance.jobs:
+            for virtual_job in instance.jobs:
+                model.addConstr((job_permutation_vars[(machine, job, virtual_job)] == 1) >> (starting_time_vars[(machine, job)] == virtual_starting_time_vars[(machine, virtual_job)]))
+
+    # Permutation equalities
+    for machine in instance.machines:
+        for job in instance.jobs:
+            model.addConstr(sum(job_permutation_vars[(machine, job, virtual_job)] for virtual_job in instance.jobs) == 1)
+
+    for machine in instance.machines:
+        for virtual_job in instance.jobs:
+            model.addConstr(sum(job_permutation_vars[(machine, job, virtual_job)] for job in instance.jobs) == 1)
+
+    # Processing time equalities
+    for virtual_job in instance.jobs:
+        for machine in instance.machines:
+            model.addConstr(virtual_processing_time_vars[(machine, virtual_job)] == sum(job_permutation_vars[(machine, job, virtual_job)] * instance.processing_times[(machine, job)] for job in instance.jobs))
+
+    # Starting time inequalities between jobs on the same machine
+    for machine in instance.machines:
+        for virtual_job, previous_virtual_job in zip(instance.jobs[1:], instance.jobs):
+            if previous_virtual_job is None:
+                continue
+            model.addConstr(virtual_starting_time_vars[machine, virtual_job] >= virtual_starting_time_vars[machine, previous_virtual_job] + virtual_processing_time_vars[machine, previous_virtual_job])
+
+    # Starting time inequalities between the same job on adjacent machines
+    for machine, previous_machine in zip(instance.machines[1:], instance.machines):
+        for job in instance.jobs:
+            model.addConstr(starting_time_vars[(machine, job)] >= starting_time_vars[(previous_machine, job)] + instance.processing_times[(previous_machine, job)])
+
+    # Order equations first two machines
+    for virtual_job in instance.jobs:
+        for job in instance.jobs:
+            model.addConstr(job_permutation_vars[(instance.machines[0], job, virtual_job)] == job_permutation_vars[(instance.machines[1], job, virtual_job)])
+
+    # Order equations last two machines
+    for virtual_job in instance.jobs:
+        for job in instance.jobs:
+            model.addConstr(job_permutation_vars[(instance.machines[-2], job, virtual_job)] == job_permutation_vars[(instance.machines[-1], job, virtual_job)])
+
+    # Starting time equations first and last machine
+    for machine in (instance.machines[0], instance.machines[-1]):
+        for virtual_job, previous_virtual_job in zip(instance.jobs[1:], instance.jobs):
+            model.addConstr(virtual_starting_time_vars[(machine, virtual_job)] == virtual_starting_time_vars[(machine, previous_virtual_job)] + virtual_processing_time_vars[(machine, previous_virtual_job)])
+
+    # Starting time equality
+    model.addConstr(virtual_starting_time_vars[(instance.machines[0], instance.jobs[0])] == 0)
+
+    # Makespan equality
+    model.addConstr(makespan_var == virtual_starting_time_vars[(instance.machines[-1], instance.jobs[-1])] + virtual_processing_time_vars[(instance.machines[-1], instance.jobs[-1])])
+
+    model.update()
+    model.setParam('TimeLimit', 100)
+    model.optimize()  # Solve the model
+
+    # Retrieve job starting times from the model
+    starting_times = {}
+
+    for machine in instance.machines:
+        for job in instance.jobs:
+            starting_times[(machine, job)] = starting_time_vars[(machine, job)].getValue()
+
+    return starting_times, model.Status == gb.GRB.OPTIMAL  # If the model is optimal, the solution is optimal
+
+
 def basic_solution(instance, job_order=None):
     if job_order is None:
         job_order = instance.jobs[:]
@@ -220,7 +323,7 @@ def visualize(instance, starting_times, optimal=False):
 def main(input_path):
     instance = Instance(input_path)
     instance.print_lower_bounds()
-    starting_times, optimal = solve(instance)
+    starting_times, optimal = solve_permutation(instance)
     visualize(instance, starting_times, optimal=optimal)
 
 
